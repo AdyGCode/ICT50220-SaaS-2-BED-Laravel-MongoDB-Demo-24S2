2644bd0dd6573a1f080c78035a6197f435766753
Adrian G <Adrian.Gould@nmtafe.wa.edu.au>
2024-10-17 16:49:25 +0800

    feat(posts): Update index view
    - change to use the app view
    - modify navigation to not show profile when not logged in

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Posts') }}
@@ -24,23 +24,19 @@
                         </div>
                     @endif
 
-                    <table class="table">
-                        <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Slug</th>
-                            <th>Actions</th>
-                        </tr>
-                        </thead>
-                        <tbody>
+                    <section class="w-full table flex flex-col gap-4">
+                        <header class="grid grid-cols-6 bg-gray-700 text-gray-100 -mx-6 px-6 py-2 mt-4">
+                            <p class="col-span-3">Title</p>
+                            <p class="col-span-2">Slug</p>
+                            <p class="col-span-1">Actions</p>
+                        </header>
+
                             @foreach($posts as $post)
-                                <tr>
-                                    <td>{{ $post->title }}</td>
-                                    <td>{{ $post->slug }}</td>
-                                    <td>
+                            <div class="flex flex-row even:bg-gray-50 border border-x-0 -mx-6 px-6 py-2 content-center">
+                                    <p class="grow content-center">{{ $post->title }}</p>
+                                    <p class="w-1/4 text-sm content-center">{{ $post->slug }}</p>
                                         <form action="{{ route('posts.destroy', $post->id) }}"
-                                            class="flex flex-row gap-4"
-                                        >
+                                              class="flex flex-row gap-1 w-1/4 justify-end content-center"                                        >
                                             <x-primary-link-button
                                                 href="{{ route('posts.show', $post->id) }}">
                                                 Show
@@ -58,20 +54,15 @@
                                                 Delete
                                             </x-secondary-button>
                                         </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                        <tfoot>
-                        <tr>
-                            <td colspan="3">
+                            </div>
+                        @endforeach
+                        <footer class="w-full">
                                 {{ $posts->links() }}
-                            </td>
-                        </tr>
-                        </tfoot>
-                    </table>
+                        </footer>
+                    </section>
+
                 </div>
             </div>
         </div>
     </div>
-</x-guest-layout>
+</x-app-layout>
